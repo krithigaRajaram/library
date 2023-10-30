132e3dc7a4950db1d1a750988fed680812ab0f03
krithiga <krithigar2020@gmail.com>
2023-10-31 00:20:04 +0530

    first commit

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express=require('express')
 const app=express()
+const expressLayouts=require('express-ejs-layouts')
 const indexRouter=require('./routes/index')
-const expressLayouts= require('express-ejs-layouts')
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-
-
-const mongoose=require('mongoose')
-mongoose.connect('mongodb://localhost:27017/library',{
-    useNewUrlParser:true
-})
-const db=mongoose.connection
+app.use('/',indexRouter);
+const mongoose=require('mongoose');
+mongoose.connect("mongodb://localhost:27017/library",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db=mongoose.connection;
 db.on('error',error=>console.error(error))
-db.once('open',()=>console.log("connection established"))
-
-app.use('/',indexRouter)
-app.listen(2090,()=>{
-    console.log("server listening")
+db.once('open',()=>{
+    console.log("connection established");
 })
+app.listen(8977,()=>{
+    console.log("server running!");
+});
